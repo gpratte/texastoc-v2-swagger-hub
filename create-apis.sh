1271e7b4c3b548dee62f41f635e5be0c363e224f
Gil Pratte <gil.pratte@mutualmobile.com>
2018-06-01 17:31:03 -0500

    figured out how to create api with path variable

--- create-apis.sh
+++ create-apis.sh
@@ -3,9 +3,42 @@
 # Set the host
 HOST=api-texastoc.herokuapp.com
 
-# Create the apis in wiremock running in heroku
-curl -X POST http://$HOST/__admin/mappings -d '{ "request": { "method": "POST", "url": "/v2/games" }, "response": { "jsonBody": {"id": "1" }, "headers": {"Content-Type": "application/json"}}}'
+# Reset the apis
+curl -X POST http://$HOST/__admin/reset
+
+# Create a new game
+curl -X POST http://$HOST/__admin/mappings -d '
+{
+   "request":{
+      "method":"POST",
+      "url":"/v2/games"
+   },
+   "response":{
+      "jsonBody":{
+         "id":"1"
+      },
+      "headers":{
+         "Content-Type":"application/json"
+      }
+   }
+}'
+
+# Get the games for a season
+curl -X POST http://$HOST/__admin/mappings -d '
+{
+   "request":{
+      "method":"GET",
+      "urlPathPattern": "/v2/games/season/([0-9]+)"
+   },
+   "response":{
+      "jsonBody":{
+         "id":"1"
+      },
+      "headers":{
+         "Content-Type":"application/json"
+      }
+   }
+}'
 
 # See all apis
 curl http://$HOST/__admin/
-
