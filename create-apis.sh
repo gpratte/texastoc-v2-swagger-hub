ac3662fcfa90c34994a6bcd9e7b1b28da3629e09
Gil Pratte <gil.pratte@mutualmobile.com>
2018-06-02 13:35:21 -0500

    enhanced json return for games of a season

--- create-apis.sh
+++ create-apis.sh
@@ -28,12 +28,44 @@ curl -X POST http://$HOST/__admin/mappings -d '
 {
    "request":{
       "method":"GET",
-      "urlPathPattern": "/v2/games/season/([0-9]+)"
+      "urlPathPattern":"/v2/games/season/([0-9]+)"
    },
    "response":{
-      "jsonBody":{
-         "id":"1"
-      },
+      "jsonBody":[
+         {
+            "id":0,
+            "date":"string",
+            "started":"string",
+            "host":"string",
+            "seasonOrdinal":0,
+            "quarterlyOrdinal":0,
+            "numPlayers":0,
+            "doubleBuyIn":true,
+            "buyIn":0,
+            "rebuyAddOn":0,
+            "annualTocAmount":0,
+            "quarterlyTocAmount":0,
+            "players":[
+               {
+                  "playerId":0,
+                  "name":"string",
+                  "place":0,
+                  "points":0,
+                  "buyIn":0,
+                  "reBuy":0,
+                  "annualToc":0,
+                  "quarterlyToc":0,
+                  "chop":0
+               }
+            ],
+            "payouts":[
+               {
+                  "place":0,
+                  "amount":0
+               }
+            ]
+         }
+      ],
       "headers":{
          "Content-Type":"application/json"
       }
